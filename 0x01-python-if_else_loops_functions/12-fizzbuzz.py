954c4788d63762ba5a66d290183e687e6cbd100b
odd-ras <knyakango@gmail.com>
2023-03-07 07:28:41 -0800

    correction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 """Prints numbers from 1 - 100 separated by space.
     For multiples of 3, print fizz.
     For multiples of both 3 and 5, print FizzBuzz
     For multiples of 5, print Buzzz.
 """
+
+
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
